a6f14e8c6f43d0dab917cd710e5a4989606937f0
Javi L <36073496+Jalagarto@users.noreply.github.com>
2020-04-16 16:47:11 +0200

    Update file_writer.py

--- file_writer.py
+++ file_writer.py
@@ -3,14 +3,18 @@ But the error went away when we modified detectron2 script, so we are going to
 try a writer, to try it as a work around"""
 
 
-def main(file = "writting_inside.py"):
+def main(file = "writting_inside.py", printer):
+
     printer = "    print('line added correctly_2')"
+    # read it
     with open(file, "r") as f:
         lines = f.readlines()
+    # write all excepting the one we don't want:
     with open(file, "w") as f:
         for line in lines:
             if line.strip("\n") != printer:
                 f.write(line)
+    # write it again (not necessary usually):
     with open(file, "a") as f:
         f.write(printer)
 
